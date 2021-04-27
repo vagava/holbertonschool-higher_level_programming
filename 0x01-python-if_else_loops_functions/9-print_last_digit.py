89895e4a8a70f645c12b4efefa716ba3f8243b2e
vagava <vagava.123@gmail.com>
2021-04-27 16:22:38 -0500

    added 'negative' variable

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/pyhton3
 def print_last_digit(number):
     if number < 0:
-        last_digit = (number * -1) % 10
+        negative = number * -1
+        last_digit = negative % 10
     else:
         last_digit = number % 10
     print("{:d}".format(last_digit), end="")
