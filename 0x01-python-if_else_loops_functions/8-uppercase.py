f560a3de223b8f6b257f6f61a2b7b817f98e9d1c
vagava <vagava.123@gmail.com>
2021-04-27 15:58:40 -0500

    fied pep8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,8 @@
 def uppercase(str):
     for position in range(len(str)):
         if 97 <= ord(str[position]) <= 122:
-            letter= ord(str[position]) - 32
+            letter = ord(str[position]) - 32
         else:
             letter = ord(str[position])
         print("{:c}".format(letter), end="")
     print()
-
