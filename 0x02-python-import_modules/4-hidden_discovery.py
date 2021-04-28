286d9f7c6586a3a164d56bc9d95738540e47568b
vagava <vagava.123@gmail.com>
2021-04-28 15:23:18 -0500

    added again the '.stor()' method

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     directory = dir(hidden_4)
+    directory.sort()
     for index in directory:
         if index[0] is not "_":
             print("{}".format(index))
