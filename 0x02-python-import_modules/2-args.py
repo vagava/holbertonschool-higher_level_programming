93afc8f4f636f6e247413ba935fd6105ab30ba05
vagava <vagava.123@gmail.com>
2021-04-28 13:55:09 -0500

    add 'if' statement after the import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-num_args = len(argv)
-if num_args == 1:
-    print("0 arguments.")
-elif num_args == 2:
-    print("{:d} argument:".format(num_args - 1))
-    print("{:d}: {:s}".format(num_args - 1, argv[1]))
-else:
-    print("{:d} arguments:".format(num_args - 1))
-    for position in range(1, num_args):
-        print("{:d}: {:s}".format(position, argv[position]))
+if __name__ == "__main__":
+    num_args = len(argv)
+    if num_args == 1:
+        print("{} arguments.".format(num_args - 1))
+    elif num_args == 2:
+        print("{:d} argument:".format(num_args - 1))
+        print("{:d}: {:s}".format(num_args - 1, argv[1]))
+    else:
+        print("{:d} arguments:".format(num_args - 1))
+        for position in range(1, num_args):
+            print("{:d}: {:s}".format(position, argv[position]))
