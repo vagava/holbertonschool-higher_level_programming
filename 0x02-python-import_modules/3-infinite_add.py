6ccc10090bae67fddd751af7dcb6a439516a6edf
vagava <vagava.123@gmail.com>
2021-04-28 15:27:35 -0500

    changed the structure of program

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def sum_all():
+if __name__ == "__main__":
+    from sys import argv
     sum = 0
     for num in range(1, len(argv)):
         sum += int(argv[num])
     print(sum)
-
-if __name__ == "__main__":
-    sum_all()
