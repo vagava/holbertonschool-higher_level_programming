5af7461b51c8f74164daee1ee73a5ebadaacb09b
vagava <vagava.123@gmail.com>
2021-04-28 17:23:08 -0500

    pythonic asignment form

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,10 +5,7 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
+    a, operator, b  = int(argv[1]), argv[2], int(argv[3])
     if operator == '+':
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == '-':
