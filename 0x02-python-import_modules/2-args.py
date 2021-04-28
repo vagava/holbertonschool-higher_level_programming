d37fa77dfebd4aa5d3f73a50d2cb59867ce54c35
vagava <vagava.123@gmail.com>
2021-04-28 14:22:43 -0500

    changed the structure of program, Created 'argument' function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__main__":
+def argument():
     num_args = len(argv)
     if num_args == 1:
         print("{} arguments.".format(num_args - 1))
@@ -11,3 +11,6 @@ if __name__ == "__main__":
         print("{:d} arguments:".format(num_args - 1))
         for position in range(1, num_args):
             print("{:d}: {:s}".format(position, argv[position]))
+
+if __name__ == "__main__":
+    argument()
