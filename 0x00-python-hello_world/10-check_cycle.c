778af320e2b98c34ba88bd7c7535437ee62a01a1
vagava <vagava.123@gmail.com>
2021-04-27 23:03:17 -0500

    add first time

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+*check_cycle - check if the list have a cycle
+*@list: pointer to head of list
+*Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *slow_p = list;
+	listint_t *fast_p = list;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
