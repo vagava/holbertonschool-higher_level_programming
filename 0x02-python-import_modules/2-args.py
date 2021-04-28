afeb5366b80fd41ab64e9d45573b10931068dae9
vagava <vagava.123@gmail.com>
2021-04-28 15:26:11 -0500

    change the structure of program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def argument():
+if __name__ == "__main__":
+    from sys import argv
     num_args = len(argv)
     if num_args == 1:
         print("{} arguments.".format(num_args - 1))
@@ -13,6 +11,3 @@ def argument():
         print("{:d} arguments:".format(num_args - 1))
         for position in range(1, num_args):
             print("{:d}: {:s}".format(position, argv[position]))
-
-if __name__ == "__main__":
-    argument()
