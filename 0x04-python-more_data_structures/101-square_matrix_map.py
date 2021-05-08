261b924ea607d9e7900832d1eba9ddec59f9f58a
vagava <vagava.123@gmail.com>
2021-05-07 23:31:52 -0500

    fixed lenght of function

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = (list(map(lambda lista: list(
-                                            map(lambda elemento:
-                                                elemento * elemento,
-                                                lista)), matrix)))
-    return new_matrix
+    return(list(map(lambda x: list(map(lambda y: y * y, x)), matrix)))
