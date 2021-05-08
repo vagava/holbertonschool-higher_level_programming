327ce44d1d177e2b03d7fc3d38acab1fb2899cb9
vagava <vagava.123@gmail.com>
2021-05-07 19:58:53 -0500

    add shebang

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for element in matrix:
-        new_matrix.append(list(map(lambda x: x**2, element)))
-    return(new_matrix)
+        new_matrix.append(list(map(lambda x: x ** 2, element)))
+    return new_matrix
