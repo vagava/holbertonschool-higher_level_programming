4917534b29254600242908da45b386de71f2768e
vagava <vagava.123@gmail.com>
2021-05-07 23:21:08 -0500

    added two lambda function

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = (list(map(lambda x: list(map(lambda y: y * y, x)), matrix)))
-    return new_matrix
+    return(list(map(lambda x: list(map(lambda y: y * y, x)), matrix)))
