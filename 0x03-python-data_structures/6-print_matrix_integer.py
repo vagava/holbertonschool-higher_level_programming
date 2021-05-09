3d11c8ba831e2281b1e0fa52443f5cea7e1b8927
vagava <vagava.123@gmail.com>
2021-05-08 22:20:25 -0500

    fixed pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         print("")
         return
     for rows in matrix:
-        for columns in range (0, len(rows)):
+        for columns in range(0, len(rows)):
             if columns == len(rows) - 1:
                 print("{:d}".format(rows[columns]), end="")
             else:
