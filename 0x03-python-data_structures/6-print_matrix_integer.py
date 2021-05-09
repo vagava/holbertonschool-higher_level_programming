c42d823461c6ddbc80fdeb68672c0b94a0d5e8b1
vagava <vagava.123@gmail.com>
2021-05-08 21:59:23 -0500

    remove format :d

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
         for columns in rows:
-            print("{:d}".format(columns), end=" ")
+            print("{:}".format(columns), end=" ")
         print("")
