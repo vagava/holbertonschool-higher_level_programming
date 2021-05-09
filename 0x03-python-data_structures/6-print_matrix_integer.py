8a37879ee0983579ef85a1c191dad5a219b42534
vagava <vagava.123@gmail.com>
2021-05-08 22:19:35 -0500

    added if condition to handle spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,9 +4,9 @@ def print_matrix_integer(matrix=[[]]):
         print("")
         return
     for rows in matrix:
-        for columns in rows:
-            if len(rows) > 1:
-                print("{:d}".format(columns), end=" ")
+        for columns in range (0, len(rows)):
+            if columns == len(rows) - 1:
+                print("{:d}".format(rows[columns]), end="")
             else:
-                print("{:d}".format(columns), end="")
+                print("{:d}".format(rows[columns]), end=" ")
         print("")
