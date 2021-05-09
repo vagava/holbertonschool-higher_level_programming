b85fcb4e8db8cdb40d362d31c83067ae0f0cede1
vagava <vagava.123@gmail.com>
2021-05-08 22:47:59 -0500

    fix pep8

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) < 1 or my_list == None:
+    if len(my_list) < 1 or my_list is None:
         return None
     my_list.sort()
     idx = len(my_list) - 1
