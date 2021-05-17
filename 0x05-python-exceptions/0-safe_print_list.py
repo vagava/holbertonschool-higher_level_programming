b6152f389740d7c955c03c15f19809030223d2b1
vagava <vagava.123@gmail.com>
2021-05-17 12:50:59 -0500

    fix the return statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for element in range(0, x+1):
+        for element in range(0, x):
             print(my_list[element], end="")
         print()
-        return element
+        return element+1
     except BaseException:
         print()
         return element
