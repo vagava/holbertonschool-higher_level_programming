849f16e5018490a9eb62aad0e67ebe78c8f6a887
vagava <vagava.123@gmail.com>
2021-05-17 12:58:19 -0500

    handle error if a element of the list is not integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for element in range(0, x):
-            print(my_list[element], end="")
+            print("{:d}".format(my_list[element]), end="")
         print()
         return element+1
     except BaseException:
