4fa99cb069488a0ab579a0170b35032e4507b356
vagava <vagava.123@gmail.com>
2021-05-18 17:51:58 -0500

    added first time

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''class Square that defines a square'''
+
+
+class Square():
+    '''
+    class Square that defines a square
+    '''
+    def __init__(self, size=0):
+        '''
+         Instantiation with optional size
+        '''
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        '''
+        Private instance attribute: size.
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''
+        Public instance method: that returns the current
+        square area.
+        '''
+        return self.__size * self.__size
+
+    def my_print(self):
+        '''
+        Public instance method  that prints in stdout
+        the square with the character #
+        '''
+        if(self.__size == 0):
+                print()
+        for rows in range(self.__size):
+            print("#"*self.__size)
