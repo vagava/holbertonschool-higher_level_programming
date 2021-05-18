6f56b44e1e1b03e2bec993b006eede6b5ffdb234
vagava <vagava.123@gmail.com>
2021-05-18 18:46:22 -0500

    added else statement in my_print function

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,7 +61,8 @@ class Square():
         '''
         if(self.__size == 0):
                 print()
-        if self.__position[1] > 0:
-            print("\n"*self.__position[1], end="")
-        for rows in range(self.__size):
-            print(" "*self.__position[0]+"#"*self.__size)
+        else:
+            if self.__position[1] > 0:
+                print("\n"*self.__position[1], end="")
+            for rows in range(self.__size):
+                print(" "*self.__position[0]+"#"*self.__size)
