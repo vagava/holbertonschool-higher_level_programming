6f982c5f1d971df97b2c06a1901989af5b91df95
vagava <vagava.123@gmail.com>
2021-05-18 16:47:48 -0500

    add class documentation and fixed pep8

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''class Square that defines a square'''
+
+
 class Square():
     '''
     a class Square that defines a square by:
@@ -9,9 +12,9 @@ class Square():
         - if size is less than 0, raise a ValueError exception with
         the message size must be >= 0
     '''
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''class Square that defines a square'''
+
+
 class Square():
     '''
     class Square that defines a square by:
@@ -8,11 +11,12 @@ class Square():
         with the message size must be an integer
         - if size is less than 0, raise a ValueError exception with the
         message size must be >= 0
-    - Public instance method: def area(self): that returns the current square area
+    - Public instance method: def area(self): that returns the current
+    square area
     '''
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
