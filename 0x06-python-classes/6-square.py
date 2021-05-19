f81e3a52ffcd9275728f197486ec5b8ff1c10ad1
vagava <vagava.123@gmail.com>
2021-05-19 16:29:53 -0500

    fixed pep8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,7 +18,7 @@ class Square():
 
         if type(position) != tuple or len(position) != 2\
                 or type(position[0]) != int or type(position[1]) != int\
-                    or position[0] < 0 or position[1] < 0:
+                or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
