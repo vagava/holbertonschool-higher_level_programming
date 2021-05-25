8787898318f81e414e5e6ab1de6d806cd7902bf2
vagava <vagava.123@gmail.com>
2021-05-24 19:55:30 -0500

    added first time

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' function that adds tow integers'''
+
+
+def add_integer(a, b=98):
+    '''add two integers'''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+# res = add_integer(2.5, 2)
+# print(res)
