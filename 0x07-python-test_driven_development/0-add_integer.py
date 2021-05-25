3e050948f1569a1a45bc3e5990cf802cdefc703c
vagava <vagava.123@gmail.com>
2021-05-24 20:00:19 -0500

    flix len of documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-''' function that adds tow integers'''
+'''
+function that adds tow integers
+    a : must be an integer
+    b : must be an integer
+if type of 'a' or 'b' is float must be casted
+to integers
+'''
 
 
 def add_integer(a, b=98):
-    '''add two integers'''
+    '''
+    add two integers
+    a : must be an integer
+    b : must be an integer
+    if type of 'a' or 'b' is float must be casted
+    to integers
+    '''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
-
-
-# res = add_integer(2.5, 2)
-# print(res)
