f53f489adc0e935a389cecd1c5542cd62bcb73c0
vagava <vagava.123@gmail.com>
2021-05-26 19:13:39 -0500

    remove the condition when the size is 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -22,8 +22,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
 
     for row in range(size):
         print("#"*size)
