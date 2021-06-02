66557575f95f78af4db9c4182812d144a5f54818
vagava <vagava.123@gmail.com>
2021-06-02 00:07:56 -0500

    fix the condition

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     returns True if the object is an instance of,
     or if the object is an instance of a class
     '''
-    return (True if isinstance(obj, a_class) else False)
+    return (False if type(obj) == a_class else issubclass(type(obj), a_class) )
