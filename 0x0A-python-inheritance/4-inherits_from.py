2f80c818476e84958f15ed1b94c894b20f4e1cd8
vagava <vagava.123@gmail.com>
2021-06-02 00:08:50 -0500

    fix pep8 style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,5 @@ def inherits_from(obj, a_class):
     returns True if the object is an instance of,
     or if the object is an instance of a class
     '''
-    return (False if type(obj) == a_class else issubclass(type(obj), a_class) )
+    return (False if type(obj) == a_class
+            else issubclass(type(obj), a_class))
