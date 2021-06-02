22ddb7315aaf77065c4f903da7560ee4a7ebf354
vagava <vagava.123@gmail.com>
2021-06-02 16:45:49 -0500

    added first time

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module from JSOn to String
+function that returns an object represented
+by a JSON
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    function that returns an object (Python
+    data structure) represented by a JSON string.
+    '''
+    return json.loads(my_str)
