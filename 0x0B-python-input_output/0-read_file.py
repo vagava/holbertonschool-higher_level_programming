41a3625bcbdf4611283c273a399ce871384262ec
vagava <vagava.123@gmail.com>
2021-06-02 15:58:17 -0500

    added first time

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module Read File
+function that reads a text file
+'''
+
+
+def read_file(filename=""):
+    '''
+    function that reads a text file
+    and prints it to stdout
+    '''
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end="")
