1ff72dab8a4dc3c0d2c3f99333501a5e6dea87db
vagava <vagava.123@gmail.com>
2021-06-02 17:06:55 -0500

    added fisrt time

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module load_from_json_file
+Creates an Object from a JSON file
+'''
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    function that creates an Object from a “JSON file”
+    '''
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.loads(file.read())
