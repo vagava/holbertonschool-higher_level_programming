3039718770747b69858794b4893f2de9bd10144c
vagava <vagava.123@gmail.com>
2021-06-02 00:18:21 -0500

    added first time

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+'''
+Module of class BaseGeometry
+'''
+
+
+class BaseGeometry:
+    '''
+    class in construction
+    '''
+    def area(self):
+        '''
+        aises an Exception with the
+        message area() is not implemented
+        '''
+        raise Exception("area() is not implemented")
