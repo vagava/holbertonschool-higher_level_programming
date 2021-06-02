0cb39c3feb221f0d2bfb7e4f7c79eb15562b4bcf
vagava <vagava.123@gmail.com>
2021-06-02 16:55:11 -0500

    added first time

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module save to JSON file
+function that save an object in a JSON text file
+'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''
+    function that writes an Object to a text file,
+    using a JSON representation.
+    '''
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(json.dumps(my_obj))
