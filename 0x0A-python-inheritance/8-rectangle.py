c99cf0685f2e1c957a8bac24192f9122b9ed319a
vagava <vagava.123@gmail.com>
2021-06-02 02:05:43 -0500

    change Super() by self

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
         '''
         constructor
         '''
-        super().integer_validator("width", width)
-        super().integer_validator("width", height)
+        self.integer_validator("width", width)
+        self.integer_validator("width", height)
         self.__width = width
         self.__height = height
