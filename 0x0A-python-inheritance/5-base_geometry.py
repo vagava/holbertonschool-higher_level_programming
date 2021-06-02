8a10881da5dc77edd6ea61248ad1ca349ce7fa82
vagava <vagava.123@gmail.com>
2021-06-02 00:00:43 -0500

    added first time

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module of function that returns True if the object is
+is an instance of, or if the object is an instance of
+a class that inherited from,the specified class.
+'''
+
+
+class BaseGeometry:
+    '''g
+    g
+    g
+    '''
+    pass
