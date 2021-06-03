af2a3cb826e1d5e4404cb3b6ed75263bb2fbdacd
vagava <vagava.123@gmail.com>
2021-06-02 19:39:52 -0500

    deberia funcinar

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+#open file
 try:
     list_args = load_from_json_file(filename)
 except Exception:
@@ -18,4 +19,5 @@ except Exception:
 # add args to the lists
 list_args += sys.argv[1:]
 
+# save to the file
 save_to_json_file(list_args, filename)
