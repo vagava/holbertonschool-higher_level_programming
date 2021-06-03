5854b78e9a1e41f0810f52793f2cbee60ada0c15
vagava <vagava.123@gmail.com>
2021-06-02 19:50:51 -0500

    replace variable filename

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,9 @@ import json
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
 # open file
 try:
-    list_args = load_from_json_file(filename)
+    list_args = load_from_json_file("add_item.json")
 except Exception:
     list_args = []
 
@@ -20,4 +19,4 @@ except Exception:
 list_args += sys.argv[1:]
 
 # save to the file
-save_to_json_file(list_args, filename)
+save_to_json_file(list_args, "add_item.json")
