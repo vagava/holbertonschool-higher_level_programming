e828c35a4ebe10b67693861855aa21edbd6156bb
vagava <vagava.123@gmail.com>
2021-06-02 22:05:43 -0500

    added first time

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+class Student
+'''
+
+
+class Student:
+    '''
+    defines a student by:
+    first_name
+    last_name
+    age
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''Constructor'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary representation'''
+        dict_j = dict()
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if attr in self.__dict__:
+                    dict_j[attr] = self.__dict__[attr]
+            return dict_j
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        '''
+        replaces all attributes of the Student instance
+        '''
+        self.json = json
+        for key in self.json:
+            self.__dict__[key] = self.json[key]
