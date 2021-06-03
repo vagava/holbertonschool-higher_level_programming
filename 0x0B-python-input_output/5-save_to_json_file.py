4878812fb4e11c6309eaca393092298abaf9e7eb
vagava <vagava.123@gmail.com>
2021-06-02 19:41:19 -0500

    remove return

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_json_file(my_obj, filename):
     using a JSON representation.
     '''
     with open(filename, 'w', encoding='utf-8') as file:
-        return json.dump(my_obj, file)
+        json.dump(my_obj, file)
