52b2d78de306afab7da627a1d6cc6e7becd018f0
vagava <vagava.123@gmail.com>
2021-06-08 21:40:51 -0500

    add test_square file

---
+++ 0x0C-python-almost_a_circle/test/test_models/test_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' Module to test rectangle class '''
+
+import unittest
+from models.square import Square
+
+
+class SquareTests(unittest.TestCase):
+    '''Test case for square'''
+
+    def test_square_init(self):
+        '''Checks instantiation of Square class'''
+        pass
+
+    def test_str(self):
+        '''Checks string representation of Square class'''
+        pass
+
+    def test_update(self):
+        '''Checks update method of Square class'''
+        pass
+
+    def to_dictionary(self):
+        '''Checks to_dictionary method of Square class'''
+        pass
