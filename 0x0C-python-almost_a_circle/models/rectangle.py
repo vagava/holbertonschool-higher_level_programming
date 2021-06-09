d4e2190909e45d7d05c53d9d62dc812a4c22baee
vagava <vagava.123@gmail.com>
2021-06-08 21:57:26 -0500

    add to dictionary method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,4 +102,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         '''returns the dictionary representation of a Rectangle'''
-        keys = ['id', 'width', 'height', 'x', 'y']
+        return {'id': self.id, 'width': self.__width, 'height': self.__height,
+                'x': self.__x, 'y': self.__y}
