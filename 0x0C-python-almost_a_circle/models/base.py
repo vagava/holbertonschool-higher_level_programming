e52162a9b2b4cd36faea221df471db8e0bb2d193
vagava <vagava.123@gmail.com>
2021-06-08 22:01:21 -0500

    add condition to to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,7 +46,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         ''' returns the JSON string representation '''
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0 \
+            or not list_dictionaries:
             return "[]"   # says "return the string" not "the empty list"
         return json.dumps(list_dictionaries)
 
