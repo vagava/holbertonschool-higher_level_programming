9cf114103b996056a2ced0e4ca03975ba61555e5
vagava <vagava.123@gmail.com>
2021-07-14 13:25:59 -0500

    Remove LEFT

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,7 +6,7 @@
 
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres
+JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
