1b0de4927e39e8640e26584d1786e9c8366b3ce5
vagava <vagava.123@gmail.com>
2021-07-13 23:52:17 -0500

    script that lists all cities contained in the database hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+CROSS JOIN states
+WHERE cities.states_id = states.id;
