a9f23c084e8ee647a0b9fda7b15270d48e81a5ca
vagava <vagava.123@gmail.com>
2021-07-14 13:18:26 -0500

    Change tv_genres.name by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -8,5 +8,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) as number_of_show
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
-ORDER BY  number_of_shows DESC;
+GROUP BY genre
+ORDER BY number_of_shows DESC;
