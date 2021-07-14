0478e1ce4050e91d403ae62d8304c315748476ed
vagava <vagava.123@gmail.com>
2021-07-14 18:07:49 -0500

    script that lists all Comedy shows in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows
+-- The tv_genres table contains only one record where name = Comedy
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+
+SELECT tv_shows.title
+FROM ((tv_show_genres
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id)
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id)
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
