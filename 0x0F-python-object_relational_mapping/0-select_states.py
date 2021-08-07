5f8be2b150191d9685fb7d724630de0b1c56d1f7
vagava <vagava.123@gmail.com>
2021-08-06 22:22:57 -0500

    add

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+'''script that lists all states from the database hbtn_0e_0_usa'''
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    args = sys.argv
+    bd = MySQLdb.connect(host="localhost", user=args[1],
+                         passwd=args[2], db=args[3], port=3306)
+    cursor = bd.cursor()
+    sql = "SELECT id, name FROM states; ORDER BY id ASC"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    bd.close()
