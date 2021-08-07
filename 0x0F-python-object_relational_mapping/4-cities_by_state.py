4d91488b5670dce40f2fe811148b0374d48f47fb
vagava <vagava.123@gmail.com>
2021-08-07 01:58:41 -0500

    script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''script that lists all cities from the database hbtn_0e_4_usa'''
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    args = sys.argv
+    # connect to Data Base
+    db = MySQLdb.connect(host='localhost', port=3360,
+                         user=args[1], passwd=args[2], db=args[3])
+    # Describe the quiery
+    sql = '''
+            SELECT cities.id, cities.name, states.name
+            FROM cities
+            LEFT JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC;
+        '''
+    # execute query
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+        data = cursor.fetchall()
+    # display data
+    for row in data:
+        print(row)
+    # close Data Base
+    db.close()
