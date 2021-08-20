0eeca4b1a3fb4318d158ce1951852226115a23ba
vagava <vagava.123@gmail.com>
2021-08-19 21:41:34 -0500

    chage the url by ''

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s "http://localhost:5000/route_3" -X DELETE
+curl -s "$1" -X DELETE
