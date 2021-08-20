ae1558ac46812ce5fc0f8af225bffd3fcd133e31
vagava <vagava.123@gmail.com>
2021-08-19 23:45:39 -0500

    fix pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 ''' function that finds a peak in a list of unsorted integers.'''
+
+
 def find_peak(list_of_integers):
     '''function that finds a peak in a list'''
     if list_of_integers == []:
         return
     list_of_integers.sort()
     return list_of_integers[-1]
-
