ecbae6f0fcf9e4b0e6e497183861e3642a6bdf66
Vanessa <vagava.123@gmail.com>
2021-08-28 13:48:11 -0500

    Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id'''
+import urllib.request
+import sys
+
+url = sys.argv[1]
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    the_page = response
+print(the_page.headers['X-Request-Id'])
