ddd61fcf0d3cb0f3534d9fcbf0711971056334b4
Vanessa <vagava.123@gmail.com>
2021-08-28 14:21:30 -0500

    use: encode('ascii') and decode(utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,8 +9,8 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     dict_ = {'email': sys.argv[1]}
-    data = urllib.parse.urlencode(dict_).encode()
+    data = urllib.parse.urlencode(dict_).encode('ascii')
     req = urllib.request.Request(url, data=data)
     with urllib.request.urlopen(url) as response:
         the_page = response.read()
-        print(the_page['email'])
+        print(the_page.decode("utf-8"))
