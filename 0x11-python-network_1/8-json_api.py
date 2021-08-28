e4e6183214fb5fd49c362e1f0a906a33d7db65ad
Vanessa <vagava.123@gmail.com>
2021-08-28 17:56:26 -0500

    add sigle quotes to key of json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     r = requests.post('http://0.0.0.0:5000/search_user', data=dict_)
     try:
         if len(r.json()) > 0:
-            print("[{}] {}".format(r.json()[id],r.json()[name],))
+            print("[{}] {}".format(r.json()['id'],r.json()['name']))
         else:
             print('No result')
     except:
