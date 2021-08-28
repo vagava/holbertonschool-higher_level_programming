4b157293a9a2ccedfe219cd3a433ad9e9f854989
Vanessa <vagava.123@gmail.com>
2021-08-28 14:59:27 -0500

    delete urllib

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Python script that takes in a URL, sends a request tothe URL
 and displays the body of the response (decoded in utf-8).'''
-from urllib import request, parse
+from urllib import request
 from urllib.error import HTTPError
 import sys
 
@@ -9,7 +9,7 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     try:
-        with urllib.request.urlopen(url) as response:
+        with request.urlopen(url) as response:
             the_page = response.read()
             print(the_page.decode("utf-8"))
     except HTTPError as e:
