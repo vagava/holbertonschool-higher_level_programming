fb77fb8a1a1b1c66a50e4c8bd5b39de1ce2d1d27
Vanessa <vagava.123@gmail.com>
2021-08-28 17:51:51 -0500

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    dict_ = {}
+    if len(sys.argv) > 1:
+        dict_['q'] = sys.argv[1]
+    else:
+        dict_['q'] = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data=dict_)
+    try:
+        if len(r.json()) > 0:
+            print("[{}] {}".format(r.json()[id],r.json()[name],))
+        else:
+            print('No result')
+    except:
+        print('Not a valid JSON')
