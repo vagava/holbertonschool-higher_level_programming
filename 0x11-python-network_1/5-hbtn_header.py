aa4c1608d3810ceba0c04efa8b9563d6d9c311ee
Vanessa <vagava.123@gmail.com>
2021-08-28 15:20:44 -0500

    add if __name__ == '__main__':

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,6 @@ and displays the value of the variable X-Request-Id in the response header'''
 import requests
 import sys
 
-r = requests.get(sys.argv[1])
-print(r.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
