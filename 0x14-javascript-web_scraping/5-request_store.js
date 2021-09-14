55666ef3031a798f0d04008e215d39455226f6ce
Vanessa <vagava.123@gmail.com>
2021-09-14 17:06:36 -0500

    script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+const URL = process.argv[2];
+const path = process.argv[3];
+
+const request = require('request');
+const fs = require('fs');
+const { encode } = require('punycode');
+
+request(URL, function (err, res, body) {
+  if (err) {
+    return console.log(err);
+  }
+  fs.writeFileSync(path, body, encoding='utf-8');
+});
