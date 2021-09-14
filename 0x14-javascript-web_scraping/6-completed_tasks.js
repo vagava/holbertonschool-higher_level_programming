7a47e19c05fe80cca135fd627ef2c6b18ec39bf2
Vanessa <vagava.123@gmail.com>
2021-09-14 18:10:49 -0500

    script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id.
+
+const URL = process.argv[2];
+
+const request = require('request');
+request(URL, function (err, res, body) {
+  if (err) {
+    return console.log(err);
+  }
+  const allUserAndTask = JSON.parse(body);
+  const taskDoneByUser = {};
+  let count = 0;
+  for (let i = 1; i <= 10; i++) {
+    count = 0;
+    for (const j of allUserAndTask) {
+      if (j.userId === i && j.completed === true) {
+        count++;
+      }
+    }
+    taskDoneByUser[i] = count;
+  }
+
+  console.log(taskDoneByUser);
+});
