f35983fa4ba23114b62f98053db673cb3168f16b
Vanessa <vagava.123@gmail.com>
2021-09-14 14:57:33 -0500

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file.
-file = process.argv[2]
+file = process.argv[2];
 fs = require('fs');
-fs.readFile(file, 'utf-8', function(err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(data);
-  });
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
