85eda6d0f2030fbf0399162e05ced90f49fbaa79
Vanessa <vagava.123@gmail.com>
2021-09-14 15:00:02 -0500

    define variables

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file.
-file = process.argv[2];
-fs = require('fs');
+const file = process.argv[2];
+const fs = require('fs');
 fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     return console.log(err);
