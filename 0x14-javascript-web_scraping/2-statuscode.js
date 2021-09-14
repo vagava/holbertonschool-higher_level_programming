9aa8e21d66e8bf7fe5e4c1faf604fa2d34e17fcb
Vanessa <vagava.123@gmail.com>
2021-09-14 15:23:01 -0500

    script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that display the status code of a GET request.
+
+const URL = process.argv[2];
+
+const https = require('https');
+https.get(URL, function (res) {
+  console.log('code: ', res.statusCode);
+  res.on('error', function (e) {
+    console.error(e);
+  });
+});
